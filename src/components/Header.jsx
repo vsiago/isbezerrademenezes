badef1a0e82b6c42c438c4ff42312b398d524311
vsiago <vsiago22@gmail.com>
2023-08-21 20:03:34 -0300

    material de design e header novo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,11 +5,13 @@ export function Header() {
   return (
     <header className={`p-5 ${bgDarkColor}`}>
       <nav className={`flex items-center justify-between`}>
-        <img
-          src="./img/SVG/logo-isbm.svg"
-          className={`w-[130px]`}
-          alt="Logo ISBM"
-        />
+        <a href="/">
+          <img
+            src="./img/SVG/logo-isbm.svg"
+            className={`w-[130px]`}
+            alt="Logo ISBM"
+          />
+        </a>
         <ul className={`hidden`}>
           <li></li>
         </ul>
