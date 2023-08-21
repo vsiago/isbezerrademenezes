8bc421183f4b29942b40e8cb12ed9ba23f735b07
vsiago <vsiago22@gmail.com>
2023-08-21 20:01:53 -0300

    material de design e header novo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,24 @@
-export default function Header() {
+import { materialDesign } from "./MaterialDesign";
+const { bgDarkColor } = materialDesign().colors;
+
+export function Header() {
   return (
-    <h1 className="bg-red-500">
-      {" "}
-      tghkjd~poghkpõdkghpokpokfsdpokgo~dkfgpokofdkgp{" "}
-    </h1>
+    <header className={`p-5 ${bgDarkColor}`}>
+      <nav className={`flex items-center justify-between`}>
+        <img
+          src="./img/SVG/logo-isbm.svg"
+          className={`w-[130px]`}
+          alt="Logo ISBM"
+        />
+        <ul className={`hidden`}>
+          <li></li>
+        </ul>
+        <img
+          src="./img/SVG/amburgIsbm.svg"
+          className={`h-6`}
+          alt="Menu Hamburg"
+        />
+      </nav>
+    </header>
   );
 }
---
+++ src/components/MaterialDesign.js
@@ -0,0 +1,12 @@
+export function materialDesign() {
+  const materialDesignISBM = {
+    colors: {
+      primaryColor: "#e0234e",
+      bgLightColor: "bg-[#F7F0F4]",
+      bgDarkColor: "bg-[#0F0D0E]",
+      bgGradient: "bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500",
+    },
+  };
+
+  return materialDesignISBM;
+}
