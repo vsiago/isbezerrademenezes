641a90c756435d484e8f35c6d37a823a98ba9fcf
vsiago <vsiago22@gmail.com>
2023-08-21 22:52:55 -0300

    hero e formatacao header pronto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
 import { materialDesign } from "./MaterialDesign";
-const { bgDarkColor } = materialDesign().colors;
+
+const { bgDarkColor, primaryColor, primaryColorActive } =
+  materialDesign().colors;
+const { textLightMenu } = materialDesign().texts;
 
 export function Header() {
   return (
-    <header className={`p-5 ${bgDarkColor}`}>
-      <nav className={`flex items-center justify-between`}>
+    <header
+      className={`p-5 px-7  ${textLightMenu} ${bgDarkColor}  absolute right-0 left-0`}
+    >
+      <nav className={`flex items-center justify-between container mx-auto`}>
         <a href="/">
           <img
             src="./img/SVG/logo-isbm.svg"
@@ -12,12 +17,72 @@ export function Header() {
             alt="Logo ISBM"
           />
         </a>
-        <ul className={`hidden`}>
-          <li></li>
+        <ul className={`hidden lg:flex gap-5`}>
+          <li>
+            <a
+              className={`px-2 py-1 transition ease delay-100 text-white hover:text-[#F43D5C] duration-300`}
+              href="/"
+            >
+              Início
+            </a>
+          </li>
+          <li>
+            <a
+              className={`px-2 py-1 transition ease-in-out delay-150 hover:text-[#F43D5C] duration-300`}
+              href="/"
+            >
+              Quem Somos
+            </a>
+          </li>
+          <li>
+            <a
+              className={`px-2 py-1 transition ease-in-out delay-150 hover:text-[#F43D5C] duration-300`}
+              href="/"
+            >
+              Programação
+            </a>
+          </li>
+          <li>
+            <a
+              className={`px-2 py-1 transition ease-in-out delay-150 hover:text-[#F43D5C] duration-300`}
+              href="/"
+            >
+              Apoiadores
+            </a>
+          </li>
+          <li>
+            <a
+              className={`px-2 py-1 transition ease-in-out delay-150 hover:text-[#F43D5C] duration-300`}
+              href="/"
+            >
+              Blog
+            </a>
+          </li>
         </ul>
+        <div className={`hidden sm:flex items-center gap-2`}>
+          <button
+            className={`text-white px-9 py-3 rounded-full transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1) hover:scale-[1.23] hover:border-5 duration-300 bg-[#e0234e]`}
+          >
+            Apoiar
+          </button>
+          <a className="ml-4" href="./">
+            <img
+              width={30}
+              src="./img/SVG/icon-instagram.svg"
+              alt="Ícone Instagram"
+            />
+          </a>
+          <a href="./">
+            <img
+              width={30}
+              src="./img/SVG/icon-facebook.svg"
+              alt="Ícone Facebook"
+            />
+          </a>
+        </div>
         <img
           src="./img/SVG/amburgIsbm.svg"
-          className={`h-6`}
+          className={`h-7 md:hidden`}
           alt="Menu Hamburg"
         />
       </nav>
--- src/components/MaterialDesign.js
+++ src/components/MaterialDesign.js
@@ -2,9 +2,15 @@ export function materialDesign() {
   const materialDesignISBM = {
     colors: {
       primaryColor: "#e0234e",
+      primaryColorActive: "#F43D5C",
+      greyColor: "#A894A0",
       bgLightColor: "bg-[#F7F0F4]",
-      bgDarkColor: "bg-[#0F0D0E]",
-      bgGradient: "bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500",
+      bgDarkColor: "bg-[#0F0D0E] lg:bg-[inherit] lg:pt-[50px]",
+      bgGradient:
+        "bg-gradient-to-br from-[#0F0D0E] to-90% via-[#7E1931]  to-[#e0234e]  ",
+    },
+    texts: {
+      textLightMenu: "text-[#BFAFB8] font-medium",
     },
   };
 
