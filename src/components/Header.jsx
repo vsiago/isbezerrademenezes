81bc0f3810585b315f700c133e635886ec82a161
vsiago <vsiago23@gmail.com>
2023-08-22 23:10:16 -0300

    btn hamburguer iniciado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,36 @@
 import { materialDesign } from "./MaterialDesign";
+import { useState } from "react";
+
 
 const { bgDarkColor, primaryColor, primaryColorActive } =
-  materialDesign().colors;
+materialDesign().colors;
 const { textLightMenu } = materialDesign().texts;
 
 export function Header() {
+  const [active, setActive] = useState(false)
+  
+  const ToggleMode = () => {
+    console.log('clicou');
+    setActive(!active)
+  }
+  
+
   return (
     <header
-      className={`p-5 px-7  ${textLightMenu} ${bgDarkColor}  absolute right-0 left-0`}
+      className={`p-4 px-7 h-[80px] ${textLightMenu} ${bgDarkColor}  absolute right-0 left-0`}
     >
       <nav className={`flex items-center justify-between container mx-auto`}>
-        <a href="/">
+        <a className={`${active ? "hidden" : ""}`} href="/">
           <img
             src="./img/SVG/logo-isbm.svg"
             className={`w-[130px] lg:w-[200px]`}
             alt="Logo ISBM"
           />
         </a>
-        <ul className={`hidden lg:flex gap-5`}>
+        <ul className={`lg:flex gap-5 sm:opacity-100 ${active ? " px-5 absolute top-0 right-0 left-0 mt-[80px] w-full h-auto pb-4 opacity-90 sm:pb-0 bg-[#0F0D0E] duration-300" : "opacity-0 h-0"}`}>
           <li>
             <a
-              className={`px-2 py-1 transition ease delay-100 text-white hover:text-[#F43D5C] duration-300`}
+              className={`px-2 py-3 border-b-2 border-black sm:border-0 w-full inline-block transition ease delay-100 text-white hover:text-[#F43D5C] duration-300`}
               href="/"
             >
               Início
@@ -28,7 +38,7 @@ export function Header() {
           </li>
           <li>
             <a
-              className={`px-2 py-1 transition ease-in-out delay-150 hover:text-[#F43D5C] duration-300`}
+              className={`px-2 py-3 border-b-2 border-black sm:border-0 w-full inline-block transition ease-in-out delay-150 hover:text-[#F43D5C] duration-300`}
               href="/"
             >
               Quem Somos
@@ -36,7 +46,7 @@ export function Header() {
           </li>
           <li>
             <a
-              className={`px-2 py-1 transition ease-in-out delay-150 hover:text-[#F43D5C] duration-300`}
+              className={`px-2 py-3 border-b-2 border-black sm:border-0 w-full inline-block transition ease-in-out delay-150 hover:text-[#F43D5C] duration-300`}
               href="/"
             >
               Programação
@@ -44,7 +54,7 @@ export function Header() {
           </li>
           <li>
             <a
-              className={`px-2 py-1 transition ease-in-out delay-150 hover:text-[#F43D5C] duration-300`}
+              className={`px-2 py-3 border-b-2 border-black sm:border-0 w-full inline-block transition ease-in-out delay-150 hover:text-[#F43D5C] duration-300`}
               href="/"
             >
               Apoiadores
@@ -52,14 +62,14 @@ export function Header() {
           </li>
           <li>
             <a
-              className={`px-2 py-1 transition ease-in-out delay-150 hover:text-[#F43D5C] duration-300`}
+              className={`px-2 py-3 w-full inline-block transition ease-in-out delay-150 hover:text-[#F43D5C] duration-300`}
               href="/"
             >
               Blog
             </a>
           </li>
         </ul>
-        <div className={`hidden sm:flex items-center gap-2`}>
+        <div className={`${active ? "flex" : "hidden"} sm:flex items-center gap-2`}>
           <button
             className={`text-white px-9 py-3 rounded-full transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1) hover:scale-[1.23] hover:border-5 duration-300 bg-[#e0234e]`}
           >
@@ -81,6 +91,7 @@ export function Header() {
           </a>
         </div>
         <img
+        onClick={ToggleMode}
           src="./img/SVG/amburgIsbm.svg"
           className={`h-7 md:hidden`}
           alt="Menu Hamburg"
