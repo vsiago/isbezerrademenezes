f0641461e0f019bb06e5c7a9f57c7788b1c024f7
vsiago <vsiago22@gmail.com>
2024-04-02 16:07:34 -0300

    agora foi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { materialDesign } from "./MaterialDesign";
 import { useState } from "react";
 
@@ -7,15 +8,39 @@ const { textLightMenu } = materialDesign().texts;
 
 export function Header() {
   const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const ToggleMode = () => {
-    console.log("clicou");
     setActive(!active);
   };
 
+  const handleMenuMobile = () => {
+    setOpen(!open)
+    ToggleMode()
+  }
+
+  const itemsMenu = [
+    {
+      nome: "Início",
+      link: "/",
+    },
+    {
+      nome: "Quem Somos",
+      link: "/quem-somos",
+    },
+    {
+      nome: "Programação",
+      link: "/programacao",
+    },
+    {
+      nome: "Apoiadores",
+      link: "/programacao",
+    },
+  ];
+
   return (
     <header
-      className={`p-4 px-7 h-[80px] flex items-center ${textLightMenu} ${bgDarkColor}  absolute right-0 left-0`}
+      className={`p-4 px-7 lg:mt-9 h-[80px] flex items-center ${textLightMenu} ${bgDarkColor}  absolute right-0 left-0`}
     >
       <nav className={`flex items-center justify-between container mx-auto`}>
         <a className={`${active ? "hidden" : ""}`} href="/">
@@ -26,7 +51,7 @@ export function Header() {
           />
         </a>
         <ul
-          className={`lg:flex gap-5 sm:opacity-100 ${
+          className={`lg:flex lg:items-center gap-5 sm:opacity-100 ${
             active
               ? " px-5 absolute top-[80px] right-0 left-0 w-full h-auto pb-4 opacity-90 sm:pb-0 bg-[#0F0D0E] duration-300"
               : "opacity-0 h-0"
@@ -79,7 +104,7 @@ export function Header() {
           <button
             className={`text-white px-9 py-3 rounded-full transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1) hover:scale-[1.23] hover:border-5 duration-300 bg-[#e0234e]`}
           >
-            Apoiar
+           - Apoiar
           </button>
           <a className="ml-2 md:ml-4" href="./">
             <img
@@ -96,12 +121,21 @@ export function Header() {
             />
           </a>
         </div>
-        <img
-          onClick={ToggleMode}
-          src="./img/SVG/amburgIsbm.svg"
-          className={`h-7 md:hidden`}
-          alt="Menu Hamburg"
-        />
+        <ul
+          onClick={handleMenuMobile}
+          className="md:hidden relative flex h-10 w-10 flex-col items-center justify-center gap-3 cursor-pointer hover:bg-[#e0234e] rounded transition duration-150 ease-in hover:transform hover:scale-105"
+        >
+          <li
+            className={`${
+              open ? "transform rotate-45 absolute" : ""
+            } w-7 h-[3px] rounded-[2px] bg-white transition duration-150 ease-in-out`}
+          ></li>
+          <li
+            className={`${
+              open ? "transform -rotate-45 absolute" : ""
+            } w-7 h-[3px] rounded-[2px] bg-white transition duration ease-in-out`}
+          ></li>
+        </ul>
       </nav>
     </header>
   );
